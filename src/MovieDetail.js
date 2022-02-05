c96255cdb599064bb744c2015ab4dc80f5e780bb
Oscar Cortez <oacortez98@gmail.com>
2022-02-04 17:41:52 -0700

    Add component and css file for MovieDetail

---
+++ src/MovieDetail.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './MovieDetail.css'
+
+const MovieDetail = ({ details }) => {
+  console.log('movie detail passed',details);
+  return (
+    <section className='movie-details'>
+      <div className='background-img-container'>
+        <img className='background-img' src={details.backdrop_path}/> 
+      </div>
+      <div className='movie-detail-container'>
+        <img className='poster-img' src={details.poster_path} />
+        <div className='movie-details'>
+          <h2 className='movie-title'>{details.title} Rating:{details.average_rating}</h2>
+          <p>{details.tagline}</p>
+          <p>{details.overview}</p>
+        </div>
+      </div>
+      
+    </section>  
+  )
+}
+
+export default MovieDetail

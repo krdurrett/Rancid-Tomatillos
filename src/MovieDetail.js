649dd37413e83433172938b13105cdf191f29c05
Oscar Cortez <oacortez98@gmail.com>
2022-02-04 18:03:29 -0700

    Change classname for css

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -4,11 +4,11 @@ import './MovieDetail.css'
 const MovieDetail = ({ details }) => {
   console.log('movie detail passed',details);
   return (
-    <section className='movie-details'>
+    <section className='movie-details-section'>
       <div className='background-img-container'>
         <img className='background-img' src={details.backdrop_path}/> 
       </div>
-      <div className='movie-detail-container'>
+      <div className='movie-details-container'>
         <img className='poster-img' src={details.poster_path} />
         <div className='movie-details'>
           <h2 className='movie-title'>{details.title} Rating:{details.average_rating}</h2>

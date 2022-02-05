97f77670f9e673c03ac9121e98a3a3211b6b88a9
Oscar Cortez <oacortez98@gmail.com>
2022-02-04 18:25:35 -0700

    Add elements to display our movie details

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './MovieDetail.css'
 
 const MovieDetail = ({ details }) => {
-  console.log('movie detail passed',details);
+
+  let genres = details.genres.join(',');
+  
   return (
     <section className='movie-details-section'>
       <div className='background-img-container'>
@@ -11,9 +13,17 @@ const MovieDetail = ({ details }) => {
       <div className='movie-details-container'>
         <img className='poster-img' src={details.poster_path} />
         <div className='movie-details'>
-          <h2 className='movie-title'>{details.title} Rating:{details.average_rating}</h2>
-          <p>{details.tagline}</p>
-          <p>{details.overview}</p>
+          <h2 className='movie-title'>{details.title} Rating:{details.average_rating} Runtime:{details.runtime}</h2>
+          <div>
+            <p>{details.tagline}</p>
+            <p>{details.overview}</p>
+            <p>{genres}</p>
+          </div>
+          <div>
+            <p>{details.budget}</p>
+            <p>{details.revenue}</p>
+            <p>{details.release_date}</p>
+          </div>
         </div>
       </div>
       

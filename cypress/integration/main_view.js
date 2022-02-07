3639bf1b79c256fd11edfeb8fe01b1865ea40ed5
Oscar Cortez <oacortez98@gmail.com>
2022-02-07 16:22:11 -0700

    Add our first it block

--- cypress/integration/main_view.js
+++ cypress/integration/main_view.js
@@ -1,5 +1,9 @@
-describe('Feedback Loop login flows', () => {
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+
+describe('Rancid tomatillo main page load', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+  it('Should be able to visit the page and view all movies', () => {
+    cy.contains('Rancid Tomatillos')
   });
 });

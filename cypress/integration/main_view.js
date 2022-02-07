055734bff8e349d04f4b37e9e40334278cf1e65c
Oscar Cortez <oacortez98@gmail.com>
2022-02-07 16:47:09 -0700

    Add test for img element

--- cypress/integration/main_view.js
+++ cypress/integration/main_view.js
@@ -5,5 +5,12 @@ describe('Rancid tomatillo main page load', () => {
   });
   it('Should be able to visit the page and view all movies', () => {
     cy.contains('Rancid Tomatillos')
+      .get('h1')
+        .contains('All Movies')
+      .get('h2')
+        .contains('Mulan')
+      .get('section')
+        .find('img')
+        .should('have.attr', 'src').should('include', "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
   });
 });

93313827bf8b75a3c5ca0801023f937d3a9d2ff7
Oscar Cortez <oacortez98@gmail.com>
2022-02-08 15:26:35 -0700

    Add all proper Route to load page and card details

--- src/App.js
+++ src/App.js
@@ -26,34 +26,6 @@ class App extends Component {
     }
   }
 
-  componentDidMount = () => {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
-      .then(data => this.setState({ allMoviesData: data.movies}))
-      .catch(() => this.setState({ error: true}))
-  }
-
-  returnHome = () => {
-    this.setState({ selectedMovie: {id: 0, 
-        title: '', 
-        poster_path: '', 
-        backdrop_path: '', 
-        release_date: '', 
-        overview: '', 
-        average_rating: 0,
-        genres: [], 
-        budget:0, 
-        revenue:0, 
-        runtime:0, 
-        tagline: '' 
-    }, error: false})
-  }
-
-  selectMovie = (id) => {
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-      .then(response => this.handleResponse(response))    
-  }
-
   handleResponse = response => {
     if (!response.ok) {
       this.setState({ error: true})
@@ -65,29 +37,23 @@ class App extends Component {
     }
   }
 
-  displayMovieDetails = () => {
-    if (this.state.selectedMovie.poster_path === '') {
-      return <AllMovieContainer movies={this.state.allMoviesData} selectMovie={this.selectMovie}/>
-    } else {
-      return <MovieDetailContainer selectedDetails={this.state.selectedMovie}/>
-    }
-  }
-
   render() {
     return (
       <main className='main'>
         <nav className='nav-bar'>
-          {/* <button onClick={this.returnHome} className='home-button'>🍿Rancid Tomatillos</button> */}
           <Link to='/' className='home-button'>🍿Rancid Tomatillos</Link>
         </nav>
-        {/* {this.state.error && <h2>Sorry, there was a problem with our network</h2>}
-        {!this.state.error && this.displayMovieDetails()} */}
-        <Route exact path='/' render={() => <AllMovieContainer movies={this.state.allMoviesData}/>} />
+        <Route exact path='/' render={() => {
+          fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+            .then(response => response.json())
+            .then(data => this.setState({ allMoviesData: data.movies, error: false}))
+            .catch(() => this.setState({ error: true}))
+          return !this.state.error ? <AllMovieContainer movies={this.state.allMoviesData}/> : <h2>Sorry, there was a problem with our network</h2> }} />
         <Route exact path='/:id' render={({ match }) => 
           {const movieId = match.params.id
             fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`)
               .then(response => this.handleResponse(response)) 
-          return <MovieDetailContainer selectedDetails={this.state.selectedMovie}/>
+            return !this.state.error ? <MovieDetailContainer selectedDetails={this.state.selectedMovie}/> : <h2>Sorry, there was a problem with our network</h2>
         }}/>
       </main>
     )

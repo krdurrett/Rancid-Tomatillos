9844f512e08022199b71bfb402f8712a29bc9c2a
Oscar Cortez <oacortez98@gmail.com>
2022-02-07 17:55:34 -0700

    Add testing for click but not completed

---
+++ cypress/integration/card_functionality.js
@@ -0,0 +1,23 @@
+describe('Rancid tomatillo main page load', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+  it('Should be able to click on a movie card to view movie details', () => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      {
+        statusCode: 201,
+        body: {
+            "id": 619592,
+            "poster_path": "https://image.tmdb.org/t/p/original//ucktgbaMSaETUDLUBp1ubGD6aNj.jpg",
+            "backdrop_path": "https://image.tmdb.org/t/p/original//eIqyISB5j99OSRZUuvdN9g2bBsS.jpg",
+            "title": "Force of Nature",
+            "average_rating": 4.4,
+            "release_date": "2020-07-02"
+            }
+      })
+      .get('.movie-cards').click(100, 20).should('not.be.visible')
+        
+        // .get('h1')
+        //   .contains('Selected movie')
+  })
+});

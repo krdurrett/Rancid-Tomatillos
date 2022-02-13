0bbfe9b39f959aaefecd986e34e0f9d8fdc4a941
Oscar Cortez <oacortez98@gmail.com>
2022-02-13 13:40:43 -0700

    Completed search functionality for searching movie titles

--- src/App.js
+++ src/App.js
@@ -11,23 +11,44 @@ class App extends Component {
     this.state = {
       allMoviesData: [],
       error: false,
+      searchValue: '', 
+      filteredMovies: []
     }
   }
 
   componentDidMount = () => {
       allMovies()
-      .then(data => this.setState({ allMoviesData: data.movies, error: false}))
+      .then(data => this.setState({ allMoviesData: data.movies, filteredMovies: data.movies ,error: false}))
       .catch(() => this.setState({ error: true}))
   }
+
+  movieSearch = (event) => {
+    event.preventDefault()
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({ [name]: value})
+    if(!this.state.searchValue) {
+      this.setState({filteredMovies: this.state.allMoviesData})
+    }
+    if(this.state.searchValue) {
+      let filterMovies = this.state.allMoviesData.filter(movie => {
+        return movie.title.toLowerCase().includes(this.state.searchValue.toLowerCase())
+      })
+      this.setState({filteredMovies: filterMovies})
+    }
+  }
+
 
   render() {
     return (
       <main className='main'>
         <nav className='nav-bar'>
           <Link to='/' className='home-button'>🍿Rancid Tomatillos</Link>
+          <input name="searchValue" type="text" className="search-bar" placeholder="Search Movie Titles" onChange={event => this.movieSearch(event)} value={this.state.searchValue} />
         </nav>
         <Route exact path='/' render={() => {
-          return !this.state.error ? <AllMovieContainer movies={this.state.allMoviesData}/> : <h2>Sorry, there was a problem with our network</h2> }} />
+          return !this.state.error ? <AllMovieContainer movies={this.state.filteredMovies}/> : <h2>Sorry, there was a problem with our network</h2> }} />
         <Route exact path='/:id' render={({ match }) => 
           {const movieId = match.params.id
           return <MovieDetailContainer movieId={movieId} />

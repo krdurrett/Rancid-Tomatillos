5555c0e889c7ab4072cd0b54675fe8d461398b54
Oscar Cortez <oacortez98@gmail.com>
2022-02-13 12:19:46 -0700

    Tested sad path error network

--- cypress/integration/preview_display.js
+++ cypress/integration/preview_display.js
@@ -4,7 +4,7 @@ describe('Play movie preview', () => {
   });
 
   it('Should be able to view movie preview', () => {
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/619592/videos',
       {
         statusCode: 201,
         body: {
@@ -24,5 +24,14 @@ describe('Play movie preview', () => {
               .find('.react-player__preview').click()
                 .get('iframe').should('have.attr', 'src').should('include', "https://www.youtube.com/embed/null?autoplay=0&mute=0&controls=1&origin=http%3A%2F%2Flocalhost%3A3000&playsinline=1&listType=playlist&playlist=aETz_dRDEys&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1")
       .url().should('include', '/694919')
+  })
+
+  it('Should have an error when preview does not load', () => {
+    cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies/619592/videos', {
+      statusCode: 500
+    })
+    .visit('http://localhost:3000/619592')
+        .get('h2')
+           .contains('Sorry, there was a problem with our network')
   })
 });

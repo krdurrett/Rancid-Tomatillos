76e217d705002cf0ce6997711ef1292a397462de
Oscar Cortez <oacortez98@gmail.com>
2022-02-13 12:20:14 -0700

    Test sad path for error network

--- cypress/integration/return_home.js
+++ cypress/integration/return_home.js
@@ -7,5 +7,15 @@ describe('Return home ', () => {
       .get('.all-movie-container')
       .url().should('equal', 'http://localhost:3000/')
   });
+
+  it('Should see error message if there is a network error', () => {
+    cy.get('.home-button').click()
+    cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 500
+    })
+    .visit('http://localhost:3000')
+      .get('h2')
+        .contains('Sorry, there was a problem with our network')
+  })
 });
 
